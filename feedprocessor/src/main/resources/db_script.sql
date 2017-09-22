e9f0fce1348a7d26ac3ffcfef91690f6e64f0c76
asgs <asgosa.eeegct@gmail.com>
2017-09-22 19:37:16 +0530

    increasing the varchar sizes of tweeter_id, and tweet message.

--- feedprocessor/src/main/resources/db_script.sql
+++ feedprocessor/src/main/resources/db_script.sql
@@ -3,8 +3,8 @@
 -- A Twitter User's information.
 
 create table if not exists tweeter (
-id varchar(10) primary key,
-location varchar(20),
+id varchar(20) primary key,
+location varchar(50),
 followers_count bigint default 0,
 friends_count bigint default 0,
 statuses_count bigint default 0,
@@ -15,8 +15,8 @@ lang varchar(5)
 
 create table if not exists tweet (
 id varchar(20) primary key,
-message varchar(140) not null,
+message varchar(300) not null,
 lang varchar(5),
 epoch_ms bigint not null,
-tweeter_id varchar(10) references tweeter(id) not null
+tweeter_id varchar(20) references tweeter(id) not null
 );
