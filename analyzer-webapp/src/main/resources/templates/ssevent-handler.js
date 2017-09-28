1d7a5b6795c07743c1f2d24691f827e8a2f92687
asgs <asgosa.eeegct@gmail.com>
2017-09-28 14:20:22 +0530

    added date controls and cleaned up the UI a little bit.

--- analyzer-webapp/src/main/resources/templates/ssevent-handler.js
+++ analyzer-webapp/src/main/resources/templates/ssevent-handler.js
@@ -1,31 +1,38 @@
+// Global state :-/
+var eventSource;
+var previousStats;
+
+var baseUrl = "http://localhost:8080/tw-stats/stream";
+
 function getEvents() {
-    var previousStats;
+    eventSource = new EventSource(constructUrl(), { withCredentials: false});
 
-    var eventSource = new EventSource("http://localhost:8080/tw-stats/stream", { withCredentials: false});
-
-    handleServerEvents(eventSource, previousStats);
+    handleServerEvents(eventSource);
     handleEventError(eventSource);
 
-    var closeButton = document.getElementById("closeConnection");
+    setOnConnectVisibility();
+
+    var closeButton = document.getElementById('closeConnection');
     attachCloseConnectionButtonHandler(closeButton);
 }
 
-function handleServerEvents(eventSource, previousStats) {
+function handleServerEvents(eventSource) {
     eventSource.onmessage = function (e) {
         var currentStats = e.data;
-        console.log("message from server - " + currentStats);
+        //console.log("message from server - " + currentStats);
         var obj = JSON.parse(currentStats);
         if (previousStats == null || currentStats != previousStats) {
             updateDom(obj);
             previousStats = currentStats;
         } else {
-            console.log("No new updates to show.");
+            console.log("No updates to show.");
         }
     }
 }
 
 function handleEventError(eventSource) {
     eventSource.onerror = function (e) {
+        // this is invoked no matter what :-/ and there's not enough info to understand the actual error.
         console.log("Error from server - " + e.type + " " + e.target + " " + e.detail + " " + e.view);
     }
 }
@@ -33,30 +40,63 @@ function handleEventError(eventSource) {
 function attachCloseConnectionButtonHandler(closeConnection) {
     closeConnection.onclick = function() {
         console.log("Terminating connection to the server.");
+        setOnDisconnectVisibility();
         eventSource.close();
     }
 }
 
 function updateDom(obj)
 {
-    document.getElementById("totalTweets").textContent = obj.totalTweets;
-    document.getElementById("totalTweeters").textContent = obj.totalTweeters;
+    document.getElementById('totalTweets').textContent = obj.totalTweets;
+    document.getElementById('totalTweeters').textContent = obj.totalTweeters;
 
-    createAndUpdateListItemsFor("topTenFollowerCounts", obj.topTenFollowerCounts);
-    createAndUpdateListItemsFor("topTenStatusCounts", obj.topTenStatusCounts);
-    createAndUpdateListItemsFor("topTenLanguages", obj.topTenLanguages);
-    createAndUpdateListItemsFor("topTenLocations", obj.topTenLocations);
+    createAndUpdateListItemsFor('topTenFollowerCounts', obj.topTenFollowerCounts);
+    createAndUpdateListItemsFor('topTenStatusCounts', obj.topTenStatusCounts);
+    createAndUpdateListItemsFor('topTenLanguages', obj.topTenLanguages);
+    createAndUpdateListItemsFor('topTenLocations', obj.topTenLocations);
 }
 
 function createAndUpdateListItemsFor(divElementId, listValues) {
     var mainDiv = document.getElementById(divElementId);
-    mainDiv.innerHTML = "";
-    var ul = document.createElement("ul");
+    mainDiv.innerHTML = ""; // clear the existing stats, if any, before updating.
+    var ul = document.createElement('ul');
     mainDiv.appendChild(ul);
     for (var count of listValues)
     {
         var countLi = document.createElement('li');
-        countLi.textContent = count == null ? "unknown": count;
+        countLi.textContent = count == null ? "unknown" : count;
         ul.appendChild(countLi);
     }
+}
+
+function constructUrl() {
+    var fromDate = document.getElementById('fromDate').value;
+    var toDate = document.getElementById('toDate').value;
+
+    var fullUrl = baseUrl;
+
+    if (fromDate.trim().length > 0) {
+        fullUrl += "?from=" + Date.parse(fromDate); // extract epoch millis.
+    }
+
+    if (toDate.trim().length > 0) {
+        if (fromDate.trim().length > 0) {
+            fullUrl += "&";
+        } else {
+            fullUrl += "?";
+        }
+        fullUrl += "to=" + Date.parse(toDate);
+    }
+    return fullUrl;
+}
+
+function setOnConnectVisibility() {
+    document.getElementById('globalStatsDiv').style = "visibility:visible";
+    document.getElementById('closeConnectionDiv').style = "visibility:visible";
+    document.getElementById('getStatsDiv').style = "visibility:hidden";
+}
+
+function setOnDisconnectVisibility() {
+    document.getElementById('closeConnectionDiv').style = "visibility:hidden";
+    document.getElementById('getStatsDiv').style = "visibility:visible";
 }
