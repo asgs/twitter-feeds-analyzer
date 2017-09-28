d5b6a22e49079da9cc35707a18556215a1eecf84
asgs <asgosa.eeegct@gmail.com>
2017-09-28 14:41:15 +0530

    IntelliJ-triggered code reformatting.

--- analyzer-webapp/src/main/java/org/asgs/twitterfeeds/AnalyzerWebappApplication.java
+++ analyzer-webapp/src/main/java/org/asgs/twitterfeeds/AnalyzerWebappApplication.java
@@ -34,12 +34,15 @@ public class AnalyzerWebappApplication extends Application<AnalyzerWebappConfigu
     environment.jersey().register(resource);
     environment.jersey().register(SseFeature.class);
     final FilterRegistration.Dynamic cors =
-            environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
     // Configure CORS parameters
     cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-    cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
-    cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+    cors.setInitParameter(
+        CrossOriginFilter.ALLOWED_HEADERS_PARAM,
+        "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+    cors.setInitParameter(
+        CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
     cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
 
     // Add URL mapping
